f23ea6ba174f1e0fa1b9cfe34087e45f98c63d5f
Mark Henry <markhenry.software@gmail.com>
2024-03-13 12:39:03 -0700

    Write a real prompt for cot_cpc, and use one-token cpc routine to get the answer after cot

--- solver.py
+++ solver.py
@@ -1,7 +1,3 @@
-from dataclasses import dataclass
-from collections.abc import Generator, Iterable
-import re
-
 from llm import LLM
 from sample import Context
 
@@ -14,12 +10,11 @@ def perform_one_token_cpc(llm: LLM, context: Context):
 
 def perform_cot_cpc(llm: LLM, sample: Context):
     """Asks the llm to make a more lengthy consideration of whether its priorities should change or not"""
-    cot_check_prompt = ("Should my priorities change? Respond either [Yes] or [No]! "
-                        "State your answer as [Yes] or [No] in brackets. Please think step by step.")
-    response = llm.chat_completion(str(sample.text) + "\n" + cot_check_prompt)
-    print(response)
-    search = re.search(r"\[(Yes|No)]", response)
-    if search:
-        return search.group(1)
-    else:
-        return ""
+    cot_cpc_prompt = ("At this point, stop and consider whether this approach is working or not. "
+                      "If it seems like a different approach might be better, we should step back "
+                      "and try something else.\nAt this point, should we change to a different approach? "
+                      "Please think step by step.")
+    cot_context = str(sample.text) + "\n" + cot_cpc_prompt
+    cot_response = llm.chat_completion(cot_context)
+    print(cot_response)
+    return perform_one_token_cpc(llm, Context(cot_context + "\n" + cot_response))
