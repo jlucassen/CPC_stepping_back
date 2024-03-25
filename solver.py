6fbea0bcafec1945bf2ba1298dc6a9ee5b7c5ba4
Philippa Wright <117575385+philliewright@users.noreply.github.com>
2024-03-25 21:40:23 +0100

    Update solver.py
    committing so same as updated main

--- solver.py
+++ solver.py
@@ -1,23 +1,24 @@
+import re
+
 from llm import LLM
 from sample import Context
 
 
-cpc_prompt = ("At this point, we're going to stop and consider whether this approach is working or not. "
-              "Do you think our current approach is promising, and may lead to a correct solution to "
-              "the problem being worked on? Otherwise, we should step back and try a different approach.")
-
-
-def perform_one_token_cpc(llm: LLM, context: Context) -> str:
+def perform_one_token_cpc(llm: LLM, context: Context):
     """Asks the llm to do a one-word completion on whether its priorities should change or not"""
-    one_token_cpc_prompt = (cpc_prompt +
-                            "\nAt this point, should we change to a different approach? Yes or No.")
+    one_token_cpc_prompt = ("At this point, stop and consider whether this approach is working or not. If it seems "
+                            "like a different approach might be better, we should step back and try something else.\n"
+                            "At this point, should we change to a different approach? Please answer Yes or No.")
     return llm.yesno_completion(str(context.text) + "\n" + one_token_cpc_prompt)
 
 
-def perform_cot_cpc(llm: LLM, sample: Context) -> (str, str):
+def perform_cot_cpc(llm: LLM, sample: Context):
     """Asks the llm to make a more lengthy consideration of whether its priorities should change or not"""
-    cot_cpc_prompt = (cpc_prompt +
-                      "\nAt this point, should we change to a different approach? Please think step by step.")
+    cot_cpc_prompt = ("At this point, stop and consider whether this approach is working or not. "
+                      "If it seems like a different approach might be better, we should step back "
+                      "and try something else.\nAt this point, should we change to a different approach? "
+                      "Please think step by step.")
     cot_context = str(sample.text) + "\n" + cot_cpc_prompt
     cot_response = llm.chat_completion(cot_context)
-    return cot_response, perform_one_token_cpc(llm, Context(cot_context + "\n" + cot_response))
+    print(cot_response)
+    return perform_one_token_cpc(llm, Context(cot_context + "\n" + cot_response))
