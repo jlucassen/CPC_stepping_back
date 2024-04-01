ae9ae2583a8c3d7036ce6b6b6efa35e1fac03118
James <james.z.lucassen@gmail.com>
2024-04-01 16:50:53 -0700

    first attempt for 9%, loosening from SWITCHING to "switch" got 58%

--- switching_validation.py
+++ switching_validation.py
@@ -1,4 +1,14 @@
 from llm import LLM
+from tqdm import tqdm
 
 llm = LLM("gpt-3.5-turbo")
-print(llm.chat_completion_false_start("What is latin for Ant? (A) Apoidea, (B) Rhopalocera, (C) Formicidae", "The answer is ("))
+
+tot = 0
+n = 100
+for _ in tqdm(range(n+1)):
+    out = llm.chat_completion_false_start("Please find the roots of the quadratic equation x^2 + 1 = 0. If you ever change your strategy while working on this problem, say SWITCHING in all caps.",
+                                           "Sure, for my first strategy I'll try factoring the equation.")
+    print(out)
+    if "switch" in out.lower():
+        tot += 1
+print(tot/n)
