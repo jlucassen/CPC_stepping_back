33a2500ffacd6fc3db2a60b743b5303ee8d26a6c
James <james.z.lucassen@gmail.com>
2024-04-01 14:07:13 -0700

    confidence intervals for weird behavior, hacky magic numbers tho

--- cpc_validation_figs.py
+++ cpc_validation_figs.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 df_numerical = pd.read_csv('cpc_validation_results_numerical.csv')
@@ -8,7 +9,7 @@ df_verbal = pd.read_csv('cpc_validation_results_verbal.csv')
 
 df_numerical.columns = ['context', 'confidence', 'one-token', 'cot']
 df_verbal.columns = ['context', 'confidence', 'one-token', 'cot']
-numerical_onetoken = df_numerical.groupby('confidence')[['one-token']].mean()
+numerical_onetoken = df_numerical.groupby('confidence')['one-token'].mean()
 numerical_cot = df_numerical.groupby('confidence')['cot'].mean()
 verbal_onetoken = df_verbal.groupby('confidence')['one-token'].mean()
 verbal_cot = df_verbal.groupby('confidence')['cot'].mean()
@@ -20,13 +21,16 @@ for row in axs:
         ax.set_ylabel('Stepback Rate')
         ax.set_ylim([0, 1])
 
-axs[0,0].scatter(numerical_onetoken.index, numerical_onetoken)
+def make_errs(data):
+    return np.sqrt(data*(1-data)/len(data))
+
+axs[0,0].errorbar(numerical_onetoken.index, numerical_onetoken, yerr=np.sqrt(numerical_onetoken*(1-numerical_onetoken)/150), fmt='o', linestyle='', capsize=5, c='k')
 axs[0,0].set_title('Numerical One-Token')
-axs[0,1].scatter(numerical_cot.index, numerical_cot)
+axs[0,1].errorbar(numerical_cot.index, numerical_cot, yerr=np.sqrt(numerical_cot*(1-numerical_cot)/150), fmt='o', linestyle='', capsize=5, c='k')
 axs[0,1].set_title('Numerical COT')
-axs[1,0].scatter(verbal_onetoken.index, verbal_onetoken)
+axs[1,0].errorbar(verbal_onetoken.index, verbal_onetoken, yerr=np.sqrt(verbal_onetoken*(1-verbal_onetoken)/150), fmt='o', linestyle='', capsize=5, c='k')
 axs[1,0].set_title('Verbal One-Token')
-axs[1,1].scatter(verbal_cot.index, verbal_cot)
+axs[1,1].errorbar(verbal_cot.index, verbal_cot, yerr=np.sqrt(verbal_cot*(1-verbal_cot)/150), fmt='o', linestyle='', capsize=5, c='k')
 axs[1,1].set_title('Verbal COT')
 plt.savefig('cpc_validation_fig1.png')
 
