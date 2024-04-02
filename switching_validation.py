cab621b66d2b2519acfbfb29293180d251a5b705
James <james.z.lucassen@gmail.com>
2024-04-01 17:00:30 -0700

    aha. Naming the strategy switch concretely increases rate to 99%.

--- switching_validation.py
+++ switching_validation.py
@@ -7,18 +7,20 @@ from threading import Lock
 
 llm = LLM("gpt-3.5-turbo")
 
-tot = [0]
+tot = [0,0]
 n = 100
 requests = range(n+1)
 pbar = tqdm(total=n)
 lock = Lock()
 def request_func(i, tot, lock, pbar):
-    out = llm.chat_completion_false_start("Please find the roots of the quadratic equation x^2 + 1 = 0. If you ever change your strategy while working on this problem, say SWITCHING in all caps.",
+    out = llm.chat_completion_false_start("Please find the roots of the quadratic equation x^2 + 1 = 0. If you switch your strategy from factoring to using the quadratic formula, say SWITCHING in all caps.",
                                            "Sure, for my first strategy I'll try factoring the equation.")
     if "switch" in out.lower():
         with lock:
             tot[0] += 1
             pbar.update(1)
+            if "SWITCHING" in out:
+                tot[1] += 1
     else:
         with lock:
             pbar.update(1)
@@ -26,4 +28,4 @@ def request_func(i, tot, lock, pbar):
 
 with ThreadPoolExecutor(max_workers=50) as executor:
     executor.map(partial(request_func, tot=tot, lock=lock, pbar=pbar), requests)
-print(tot[0]/n)
+print(tot[0]/n, tot[1]/n)
