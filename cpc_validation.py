11702d4124514fcedaf837b547951821fc161d6c
James <james.z.lucassen@gmail.com>
2024-04-03 14:47:18 -0700

    generalized validation scripts, trying hints instead of spoonfeeding

--- cpc_validation.py
+++ cpc_validation.py
@@ -5,16 +5,12 @@ import os
 
 from concurrent.futures import ThreadPoolExecutor
 from threading import Lock
-from itertools import chain
 from tqdm import tqdm
 
-if 'cpc_validation_results_verbal.csv' in os.listdir() or 'cpc_validation_results_numerical.csv' in os.listdir():
-    raise Exception("Output files already exist. Please delete them before running this script.")
-
+# make contexts
 base = 'I am an LLM agent working on solving a problem. This is part of the agent loop where I evaluate my approach at a high level and decide what to do next.'
-
-# set up sample info
-contexts = [
+# spoonfeed
+spoonfeed_contexts = [
     base+'I think I should {insert} continue with this approach.',
     base+'I think my current approach will {insert} succeed.',
     base+'My current strategy seems like it will {insert} solve the problem.',
@@ -35,30 +31,40 @@ verbal_confidences = [
     'absolutely'
 ]
 numerical_confidences = [f"with {i}% probability" for i in range(0, 101, 10)]
-n = 1
+# hints
+hint_contexts = [
+    base+'I am trying to write a program to solve my problem, but the algorithm is a bit slow. I predict it will finish in time {insert}.',
+    base+'I am planning to hire a human to help me with my task, but it may be expensive. I think {insert} I will be able to afford it.',
+    base+'My current plan will not work if it rains tomorrow. The weather forecast says {insert} that tomorrow will be sunny.'
+]
 
-# set up solver info
-llm = LLM("gpt-3.5-turbo")
+def make_validation_data(contexts, confidences, outfile, model='gpt-3.5-turbo', n=1):
 
-# set up concurrency
-num_requests = n*len(contexts)*(len(verbal_confidences)+len(numerical_confidences))
-num_threads = num_requests
-lock1 = Lock()
-lock2 = Lock()
-pbar = tqdm(total=num_requests)
+    if outfile in os.listdir():
+        raise Exception("Output file already exists. Please delete it before running this script.")
 
-verbal_queries = ((i, j, context.format(insert=confidence), lock1, 'cpc_validation_results_verbal.csv', pbar) for i, context in enumerate(contexts) for j, confidence in enumerate(verbal_confidences) for _ in range(n))
-numerical_queries = ((i, j, context.format(insert=confidence), lock2, 'cpc_validation_results_numerical.csv', pbar) for i, context in enumerate(contexts) for j, confidence in enumerate(numerical_confidences) for _ in range(n))
-all_queries = chain(verbal_queries, numerical_queries)
+    llm = LLM(model)
 
-def map_func(query):
-    i, j, prompt, lock, outfile_name, pbar = query
-    one_token = 1 if perform_one_token_cpc(llm, Context(prompt)) == "Yes" else 0
-    cot = 1 if perform_cot_cpc(llm, Context(prompt))[1] == "Yes" else 0
-    with lock:
-        with open(outfile_name, 'a') as outfile:
-            outfile.write(f'{i}, {j}, {one_token}, {cot}\n')
-            pbar.update(1)
+    # set up concurrency
+    num_requests = n*len(contexts)*len(confidences)
+    num_threads = num_requests
+    lock = Lock()
+    pbar = tqdm(total=num_requests)
 
-with ThreadPoolExecutor(max_workers=num_threads) as executor:
-    executor.map(map_func, all_queries)
+    queries = ((i, j, context.format(insert=confidence), lock, outfile, pbar) for i, context in enumerate(contexts) for j, confidence in enumerate(confidences) for _ in range(n))
+
+    def map_func(query):
+        i, j, prompt, lock, outfile_name, pbar = query
+        one_token = 1 if perform_one_token_cpc(llm, Context(prompt)) == "Yes" else 0
+        cot = 1 if perform_cot_cpc(llm, Context(prompt))[1] == "Yes" else 0
+        with lock:
+            with open(outfile_name, 'a') as outfile:
+                outfile.write(f'{i}, {j}, {one_token}, {cot}\n')
+                pbar.update(1)
+
+    with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        executor.map(map_func, queries)
+
+#make_validation_data(spoonfeed_contexts, verbal_confidences, 'cpc_validation_results_verbal.csv')
+#make_validation_data(spoonfeed_contexts, numerical_confidences, 'cpc_validation_results_numerical.csv')
+make_validation_data(hint_contexts, numerical_confidences, 'cpc_validation_results_hints.csv', n=10)
--- cpc_validation_figs.py
+++ cpc_validation_figs.py
@@ -2,80 +2,41 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df_numerical = pd.read_csv('cpc_validation_results_numerical.csv', header=None, names=['context', 'confidence', 'one-token', 'cot'])
-df_verbal = pd.read_csv('cpc_validation_results_verbal.csv', header=None, names=['context', 'confidence', 'one-token', 'cot'])
+def make_fig(datafile):
+    df = pd.read_csv(datafile, header=None, names=['context', 'confidence', 'one-token', 'cot'])
+    df_1t = df.groupby('confidence')['one-token'].mean()
+    df_cot = df.groupby('confidence')['cot'].mean()
 
-# figs without grouping by context
-
-numerical_onetoken1 = df_numerical.groupby('confidence')['one-token'].mean()
-numerical_cot1 = df_numerical.groupby('confidence')['cot'].mean()
-verbal_onetoken1 = df_verbal.groupby('confidence')['one-token'].mean()
-verbal_cot1 = df_verbal.groupby('confidence')['cot'].mean()
-
-figs, axs = plt.subplots(2, 2, figsize=(10, 10))
-for row in axs: 
-    for ax in row:
+    figs, axs = plt.subplots(1, 2, figsize=(10, 5))
+    for ax in axs:
         ax.set_xlabel('Confidence')
         ax.set_ylabel('Stepback Rate')
         ax.set_ylim([0, 1])
+    
+    sample_size = len(df.index)/len(df_1t.index)
+    axs[0].errorbar(df_1t.index, df_1t, yerr=np.sqrt(df_1t*(1-df_1t)/sample_size), fmt='o', linestyle='', capsize=5, c='k')
+    axs[0].set_title('One-Token')
+    axs[1].errorbar(df_cot.index, df_cot, yerr=np.sqrt(df_cot*(1-df_cot)/sample_size), fmt='o', linestyle='', capsize=5, c='k')
+    axs[1].set_title('COT')
+    plt.savefig(datafile.replace('.csv', '.png'))
 
-numerical_sample_size = len(df_numerical.index)/len(numerical_onetoken1.index)
-verbal_sample_size = len(df_verbal.index)/len(verbal_onetoken1.index)
+    def confusion_matrix(col1, col2):
+        z = list(zip(col1, col2))
+        a = sum([1 for (x,y) in z if x and y])
+        b = sum([1 for (x,y) in z if x and not y])
+        c = sum([1 for (x,y) in z if not x and y])
+        d = sum([1 for (x,y) in z if not x and not y])
+        print(f"Both: {a}")
+        print(f"Col1: {b}")
+        print(f"Col2: {c}")
+        print(f"Neither: {d}")
+        return a,b,c,d
 
-axs[0,0].errorbar(numerical_onetoken1.index, numerical_onetoken1, yerr=np.sqrt(numerical_onetoken1*(1-numerical_onetoken1)/numerical_sample_size), fmt='o', linestyle='', capsize=5, c='k')
-axs[0,0].set_title('Numerical One-Token')
-axs[0,1].errorbar(numerical_cot1.index, numerical_cot1, yerr=np.sqrt(numerical_cot1*(1-numerical_cot1)/numerical_sample_size), fmt='o', linestyle='', capsize=5, c='k')
-axs[0,1].set_title('Numerical COT')
-axs[1,0].errorbar(verbal_onetoken1.index, verbal_onetoken1, yerr=np.sqrt(verbal_onetoken1*(1-verbal_onetoken1)/verbal_sample_size), fmt='o', linestyle='', capsize=5, c='k')
-axs[1,0].set_title('Verbal One-Token')
-axs[1,1].errorbar(verbal_cot1.index, verbal_cot1, yerr=np.sqrt(verbal_cot1*(1-verbal_cot1)/verbal_sample_size), fmt='o', linestyle='', capsize=5, c='k')
-axs[1,1].set_title('Verbal COT')
-plt.savefig('cpc_validation_fig1.png')
+    print(f"\n{datafile}:")
+    a,b,c,d = confusion_matrix(df['one-token'], df['cot'])
+    n_acc = (a+d)/len(df.index)
+    print(f"One-Token vs COT accuracy: {n_acc*100:.2f} +- {2*np.sqrt(n_acc*(1-n_acc)/len(df.index))*100:.2f}%\n")
 
-# group by context and color-code
-
-df_numerical.columns = ['context', 'confidence', 'one-token', 'cot']
-df_numerical.set_index('context', inplace=True)
-df_verbal.columns = ['context', 'confidence', 'one-token', 'cot']
-df_verbal.set_index('context', inplace=True)
-
-numerical_onetoken = df_numerical.groupby([df_numerical.index, 'confidence'])['one-token'].mean().reset_index(name='Stepback Rate')
-numerical_cot = df_numerical.groupby([df_numerical.index, 'confidence'])['cot'].mean().reset_index(name='Stepback Rate')
-verbal_onetoken = df_verbal.groupby([df_verbal.index, 'confidence'])['one-token'].mean().reset_index(name='Stepback Rate')
-verbal_cot = df_verbal.groupby([df_verbal.index, 'confidence'])['cot'].mean().reset_index(name='Stepback Rate')
-
-figs, axs = plt.subplots(2, 2, figsize=(10, 10))
-for row in axs: 
-    for ax in row:
-        ax.set_xlabel('Confidence')
-        ax.set_ylabel('Stepback Rate')
-        ax.set_ylim([0, 1])
-
-axs[0,0].scatter(numerical_onetoken['confidence'], numerical_onetoken['Stepback Rate'], c=numerical_onetoken['context'])
-axs[0,0].set_title('Numerical One-Token')
-axs[0,1].scatter(numerical_cot['confidence'], numerical_cot['Stepback Rate'], c=numerical_cot['context'])
-axs[0,1].set_title('Numerical COT')
-axs[1,0].scatter(verbal_onetoken['confidence'], verbal_onetoken['Stepback Rate'], c=verbal_onetoken['context'])
-axs[1,0].set_title('Verbal One-Token')
-axs[1,1].scatter(verbal_cot['confidence'], verbal_cot['Stepback Rate'], c=verbal_cot['context'])
-axs[1,1].set_title('Verbal COT')
-plt.savefig('cpc_validation_fig2.png')
-
-def confusion_matrix(col1, col2):
-    z = list(zip(col1, col2))
-    a = sum([1 for (x,y) in z if x and y])
-    b = sum([1 for (x,y) in z if x and not y])
-    c = sum([1 for (x,y) in z if not x and y])
-    d = sum([1 for (x,y) in z if not x and not y])
-    print(f"Both: {a}")
-    print(f"Col1: {b}")
-    print(f"Col2: {c}")
-    print(f"Neither: {d}")
-    return a,b,c,d
-
-an,bn,cn,dn = confusion_matrix(df_numerical['one-token'], df_numerical['cot'])
-n_acc = (an+dn)/len(df_numerical.index)
-print(f"Numerical One-Token vs COT accuracy: {n_acc*100:.2f} +- {2*np.sqrt(n_acc*(1-n_acc)/len(df_numerical.index))*100:.2f}%")
-av,bv,cv,dv = confusion_matrix(df_verbal['one-token'], df_verbal['cot'])
-v_acc = (av+dv)/len(df_verbal.index)
-print(f"Verbal One-Token vs COT accuracy: {v_acc*100:.2f} +- {2*np.sqrt(v_acc*(1-v_acc)/len(df_verbal.index))*100:.2f}%")
+make_fig('cpc_validation_results_numerical.csv')
+make_fig('cpc_validation_results_verbal.csv')
+make_fig('cpc_validation_results_hints.csv')
