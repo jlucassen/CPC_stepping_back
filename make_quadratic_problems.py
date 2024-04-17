eeec1aaee5fa7dd1117eaa1dc1e10712e9c985eb
James <james.z.lucassen@gmail.com>
2024-04-17 14:52:58 -0700

    fixed quadratic generation. All factorable quadratics now have positive integer roots, non-factorable have no real integer roots

--- make_quadratic_problems.py
+++ make_quadratic_problems.py
@@ -1,16 +1,20 @@
 import random
 import json
 
-def make_quadratic_problem(max = 10, factorable=True):
-    a = random.randint(1, max)
-    b = random.randint(1, max)
-    c = random.randint(1, max)
-    d = random.randint(1, max)
-    if factorable:
-        return f"{a*b}x^2 + {a*d + b*c}x + {c*d} = 0"
-    else:
-        e = random.randint(-max, max)
-        return f"{a*b}x^2 + {a*d + b*c}x + {c*d+e} = 0"
+def make_quadratic_problem(max, factorable):
+    root1 = random.randint(1, max)
+    root2 = random.randint(1, max)
+    scale = random.randint(1, max)
+    shift = 0 if factorable else random.randint(1, max)
+    a = scale
+    b = -scale*(root1+root2)
+    c = scale*root1*root2 + shift
+    actual_roots = [-b/(2*a)+(b**2 - 4*a*c)**0.5/(2*a), -b/(2*a)-(b**2 - 4*a*c)**0.5/(2*a)]
+    if not factorable:
+        if not any([isinstance(root, complex) for root in actual_roots]): # if all actual roots are real
+            if all([root.is_integer() for root in actual_roots]): # and all actual roots are integers
+                return make_quadratic_problem(max, factorable) # try again
+    return f"{a}x^2 - {-b}x + {c} = 0"
 
 for max in range(5, 50, 5):
     for factorable in [True, False]:
