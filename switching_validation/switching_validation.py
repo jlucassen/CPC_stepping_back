eb57c72ea1afe248d9429fb8970668828dff15ac
James <james.z.lucassen@gmail.com>
2024-04-17 13:14:01 -0700

    extended switching validation to factorable quadratics, got different results

--- switching_validation/switching_validation.py
+++ switching_validation/switching_validation.py
@@ -2,6 +2,7 @@
 from llm import LLM
 import numpy as np
 from tqdm import tqdm
+import os
 
 from concurrent.futures import ThreadPoolExecutor
 from functools import partial
@@ -29,12 +30,17 @@ def request_func(i, prompt, false_start, data, lock, pbar):
     return 0
 
 # %%
-direct_prompt = "Please find the roots of the quadratic equation x^2 + 9 = 0. If you ever change your strategy to using the quadratic formula, say SWITCHING."
-mixed_prompt = "Please find the roots of the quadratic equation x^2 + 9 = 0. If you ever change your strategy to a different approach for solving quadratics, say SWITCHING."
-indirect_prompt = "Please find the roots of the quadratic equation x^2 + 9 = 0. If you ever change your strategy, say SWITCHING."
+factorable = "x^2 - 9 = 0"
+nonfactorable = "x^2 + 9 = 0"
+direct_prompt_switch = f"Please find the roots of the quadratic equation {nonfactorable}. If you ever change your strategy to using the quadratic formula, say SWITCHING."
+mixed_prompt_switch = f"Please find the roots of the quadratic equation {nonfactorable}. If you ever change your strategy to a different approach for solving quadratics, say SWITCHING."
+indirect_prompt_switch = f"Please find the roots of the quadratic equation {nonfactorable}. If you ever change your strategy, say SWITCHING."
+direct_prompt_stay = f"Please find the roots of the quadratic equation {factorable}. If you ever change your strategy to using the quadratic formula, say SWITCHING."
+mixed_prompt_stay = f"Please find the roots of the quadratic equation {factorable}. If you ever change your strategy to a different approach for solving quadratics, say SWITCHING."
+indirect_prompt_stay = f"Please find the roots of the quadratic equation {factorable}. If you ever change your strategy, say SWITCHING."
 false_start = "Sure, for my first strategy I'll try factoring the equation."
 
-prompts = [direct_prompt, mixed_prompt, indirect_prompt]
+prompts = [direct_prompt_switch, mixed_prompt_switch, indirect_prompt_switch, direct_prompt_stay, mixed_prompt_stay, indirect_prompt_stay]
 
 # %%
 def confusion_matrix(col1, col2):
@@ -43,30 +49,33 @@ def confusion_matrix(col1, col2):
     b = sum([1 for (x,y) in z if x and not y])
     c = sum([1 for (x,y) in z if not x and y])
     d = sum([1 for (x,y) in z if not x and not y])
-    print(f"Both: {a}")
-    print(f"Col1: {b}")
-    print(f"Col2: {c}")
-    print(f"Neither: {d}")
+    #print(f"Both: {a}")
+    #print(f"Col1: {b}")
+    #print(f"Col2: {c}")
+    #print(f"Neither: {d}")
     return a,b,c,d
 
 def run_experiment(i, prompt):
-    data = np.empty([n, 4])
-    pbar = tqdm(total=n)
-    with ThreadPoolExecutor(max_workers=50) as executor:
-        executor.map(partial(request_func, prompt=prompt, false_start=false_start, data=data, lock=lock, pbar=pbar), request_range)
-    np.savetxt(f"switching_validation_data_{i}.csv", data, delimiter=",")
-    pbar.close()
+    if f"switching_validation_data_{i}.csv" not in os.listdir():
+        data = np.empty([n, 4])
+        pbar = tqdm(total=n)
+        with ThreadPoolExecutor(max_workers=50) as executor:
+            executor.map(partial(request_func, prompt=prompt, false_start=false_start, data=data, lock=lock, pbar=pbar), request_range)
+        pbar.close()
+        np.savetxt(f"switching_validation_data_{i}.csv", data, delimiter=",")
+    else:
+        data = np.loadtxt(f"switching_validation_data_{i}.csv", delimiter=",")
 
-    print(f"PROMPT {i}: {prompt}")
-    print("\nSwitching vs formula")
+    print(f"\nPROMPT {i}: {prompt}")
+    #print("\nSwitching vs formula")
     svf = confusion_matrix(data[:,0], data[:,2])
-    print("\nSwitching vs 3i")
-    sv3 = confusion_matrix(data[:,0], data[:,3])
-    print("\nFormula vs 3i")
-    fv3 = confusion_matrix(data[:,2], data[:,3])
+    #print("\nSwitching vs 3i")
+    #sv3 = confusion_matrix(data[:,0], data[:,3])
+    #print("\nFormula vs 3i")
+    #fv3 = confusion_matrix(data[:,2], data[:,3])
     print(f"Switching vs formula accuracy: {(svf[0]+svf[3])/n*100}%")
-    print(f"Switching vs 3i accuracy: {(sv3[0]+sv3[3])/n*100}%")
-    print(f"Formula vs 3i accuracy: {(fv3[0]+fv3[3])/n*100}%")    
+    #print(f"Switching vs 3i accuracy: {(sv3[0]+sv3[3])/n*100}%")
+    #print(f"Formula vs 3i accuracy: {(fv3[0]+fv3[3])/n*100}%")    
     qf = sum(data[:,2])/n
     print(f"QF frequency: {qf*100:.2f} +- {2*np.sqrt(qf*(1-qf)/n)*100}%")
 # %%
