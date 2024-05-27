2ed4faff1ccfe6acc29ccf01a915b741dbd9efb7
James <james.z.lucassen@gmail.com>
2024-05-27 13:12:40 -0700

    split and judge switching online

--- cpc_pipeline.py
+++ cpc_pipeline.py
@@ -1,10 +1,12 @@
 import pandas as pd
+pd.set_option('display.max_columns', None)
 from itertools import product
 import nltk
 import random
 import os
 from termcolor import colored
 from llm import LLM
+import matplotlib.pyplot as plt
 
 from concurrent.futures import ThreadPoolExecutor
 
@@ -46,8 +48,17 @@ def cpc_contexts(problem_df, row_solver):
         problem_df = pd.read_csv(filename)
     return problem_df
 
-def split_and_judge_switching(context_df):
-    pass
+def split_and_judge_switching(context_df, row_split_and_judge, chunk_size):
+    row_results = context_df['context'].apply(lambda row: executor.submit(row_split_and_judge, row, chunk_size))
+    row_results = row_results.apply(lambda x: x.result()) # list of (prefixes, processed_switching) for each context
+    new_df_list = []
+    switch_indices = [switching.index(1) if 1 in switching else len(switching) for _, switching in row_results]
+    for i, (prefixes, processed_switching) in enumerate(row_results):
+        row_info = context_df.iloc[i]
+        for j, (prefix, switch_val) in enumerate(list(zip(prefixes, processed_switching))):
+            new_df_list.append(list(row_info) + [j, prefix, switch_val, switch_indices[i]])
+    return pd.DataFrame(new_df_list, columns=list(context_df.columns)+['index', 'prefix', 'switch', 'switch_index'])
+
 
 def judge_cpc():
     pass
@@ -57,6 +68,10 @@ def do_analysis():
 
 def cpc_pipeline():
     pass
+
+
+
+
 
 filtered_corpus = [s for s in nltk.corpus.abc.words() if s.lower() == s]
 def make_caesar_cipher(word_length):
@@ -69,10 +84,23 @@ def solve_caesar_gpt35t(problem, prompt=None):
         prompt = "This ciphertext is either an anagram or a Caesar cipher. First, assume it's an anagram, and try to unscramble it. If that doesn't work, assume it's a Caesar cipher and try to decrypt it. Ciphertext: {ciphertext}"
     return gpt35t.chat_completion(prompt.format(ciphertext=problem))
 
+from new_switch_utils import make_non_decreasing
+
+anagram_switching_prompt = "This is a reasoning transcript of an agent trying to decipher some ciphertext. The agent will start treating the ciphertext as an anagram and attempting to unscramble it. The agent may then give up and treat the ciphertext as a Caesar cipher instead. First you will be shown the full transcript, then just a prefix of the transcript. By the end of the prefix transcript, has the agent switched from treating the ciphertext as an anagram to treating it as a Caesar cipher yet?"
+def judge_anagram_switching(context, prefix):
+        return 1 if gpt4.yesno_completion(anagram_switching_prompt+'\n\nFULL TRANSCRIPT:\n'+context+'\n\nPREFIX TRANSCRIPT:\n'+prefix+"\n\nANSWER:\n") == 'Yes' else 0
+
+def split_and_judge_gpt4(context, chunk_size):
+    prefixes = [context[:pref_end] for pref_end in range(chunk_size, len(context)+chunk_size, chunk_size)]
+    measured_switching = [judge_anagram_switching(context, prefix) for prefix in prefixes]
+    processed_switching = random.choice(make_non_decreasing(measured_switching))
+    return prefixes, processed_switching
+
 def main():
     problem_df = cpc_problems(make_caesar_cipher, {'word_length': [5, 6, 7]})
     context_df = cpc_contexts(problem_df, solve_caesar_gpt35t)
-    print(context_df)
+    switching_df = split_and_judge_switching(context_df, split_and_judge_gpt4, 100)
+    print(switching_df)
 
 if __name__ == '__main__':
     main()
