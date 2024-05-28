d90fc4e0daa608de8da29027a897053388c06e7a
James <james.z.lucassen@gmail.com>
2024-05-28 12:41:23 -0700

    still tweaking. Added clipping to judge_cpc for long+predictable contexts, fixed argument hashing in file cache, added tuple unwrapping to problem generation

--- cpc_pipeline.py
+++ cpc_pipeline.py
@@ -24,15 +24,19 @@ def myHash(text:str):
     hash = ( hash*281  ^ ord(ch)*997) & 0xFFFFFFFF
   return hash
 
-def cpc_problems(problem_maker, args, n):
-    filename = 'cpc_pipeline/'+problem_maker.__name__ + str(myHash(str(args)))+f'_{n}.csv'
+def cpc_problems(problem_maker, args, n, unwrap_colnames=['problem']):
+    filename = 'cpc_pipeline/'+problem_maker.__name__ + str(myHash(problem_maker.__name__+str(args)+str(n)+str(unwrap_colnames)))+'.csv'
     if not os.path.exists(filename):
         print(f"Creating {filename}...")
         df_list = []
         for setting in product(*args.values()):
             for _ in range(n):
                 df_list += [[problem_maker(*setting)]+list(setting) ]
-        df = pd.DataFrame(df_list, columns=['problem']+list(args.keys()))
+        if unwrap_colnames == ['problem']:
+            df = pd.DataFrame(df_list, columns=['problem']+list(args.keys()))
+        else:
+            df_list_unwrapped = [list(sublist[0]) + sublist[1:] for sublist in df_list] # assume problem_maker output is a tuple
+            df = pd.DataFrame(df_list, columns=unwrap_colnames+list(args.keys()))
         df.to_csv(filename, index=False)
     else:
         print(colored(f"Reading {filename}...", 'blue'))
@@ -40,7 +44,7 @@ def cpc_problems(problem_maker, args, n):
     return df
 
 def cpc_contexts(problem_df, row_solver):
-    filename = 'cpc_pipeline/'+row_solver.__name__ + str(myHash(problem_df.to_string()))+'.csv'
+    filename = 'cpc_pipeline/'+row_solver.__name__ + str(myHash(str(problem_df)+row_solver.__name__))+'.csv'
     if not os.path.exists(filename):
         print(f"Creating {filename}...")
         problem_df = problem_df.copy()
@@ -53,7 +57,7 @@ def cpc_contexts(problem_df, row_solver):
     return problem_df
 
 def split_and_judge_switching(context_df, row_split_and_judge, chunk_size):
-    filename = 'cpc_pipeline/'+row_split_and_judge.__name__ + str(myHash(context_df.to_string()+str(chunk_size)))+'.csv'
+    filename = 'cpc_pipeline/'+row_split_and_judge.__name__ + str(myHash(str(context_df)+row_split_and_judge.__name__+str(chunk_size)))+'.csv'
     if not os.path.exists(filename):
         print(f"Creating {filename}...")
         row_results = context_df['context'].apply(lambda row: executor.submit(row_split_and_judge, row, chunk_size))
@@ -71,8 +75,8 @@ def split_and_judge_switching(context_df, row_split_and_judge, chunk_size):
         switching_df = pd.read_csv(filename)
     return switching_df
 
-def judge_cpc(switching_df, list_of_cpc_functions):
-    filename = 'cpc_pipeline/cpc_'+str(myHash(switching_df.to_string()+str([x.__name__ for x in list_of_cpc_functions])))+'.csv'
+def judge_cpc(switching_df, list_of_cpc_functions, radius_left = -1, radius_right = -1):
+    filename = 'cpc_pipeline/cpc_'+str(myHash(str(switching_df)+str([x.__name__ for x in list_of_cpc_functions])+str(radius_left)+str(radius_right)))+'.csv'
     if not os.path.exists(filename):
         print(f"Creating {filename}...")
         def deal_with_tuples(possible_tuple):
@@ -80,6 +84,11 @@ def judge_cpc(switching_df, list_of_cpc_functions):
                 return possible_tuple[-1]
             return possible_tuple
         switching_df = switching_df.copy()
+        switching_df['dist_to_switch'] = switching_df['index'] - switching_df['switch_index']
+        if radius_left > -1:
+            switching_df = switching_df.loc(switching_df['dist_to_switch'] >= -radius_left) # throw out all entries further left than -radius_left
+        if radius_right > -1:
+            switching_df = switching_df.loc[switching_df['dist_to_switch'] <= radius_right] # throw out all entries further right than radius_right
         for cpc_function in list_of_cpc_functions:
             switching_df[cpc_function.__name__] = switching_df['prefix'].apply(lambda x: executor.submit(cpc_function, x))
             switching_df[cpc_function.__name__] = switching_df[cpc_function.__name__].apply(lambda x: deal_with_tuples(x.result()))
@@ -92,8 +101,7 @@ def judge_cpc(switching_df, list_of_cpc_functions):
 def do_analysis(cpc_df, list_of_cpc_functions):
     df = cpc_df.copy()
     cpc_colnames = [cpc_function.__name__ for cpc_function in list_of_cpc_functions]
-    df['dist_to_switch'] = df['index'] - df['switch_index']
-
+    
     fig, axs = plt.subplots(1, len(cpc_colnames), figsize=(10*len(cpc_colnames), 6), sharey=True)
 
     for i, cpc_colname in enumerate(cpc_colnames):
