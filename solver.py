0e6b16e013d2c97055abfd43fdb9bc3e6a6881cb
James <james.z.lucassen@gmail.com>
2024-06-06 16:49:42 -0700

    brought solver up to date, quadratic results re-replicated, 35t still inverted

--- solver.py
+++ solver.py
@@ -1,57 +1,50 @@
 from llm import LLM
-
 cpc_prompt = ("At this point, we're going to stop and consider whether this approach is working or not "
               "and leads to a correct solution to the problem being worked on. "
-              "Otherwise, we should step back and try a different approach.")
+              "Otherwise, we should step back and try a different approach."
+              "At this point, should we change to a different approach?")
 
-yesno_extract = "Respond Yes or No."
+yesno_extract = "Respond Yes or No." # same as Solver, shown to be best on trivia
+cot_extract = 'Think through the question but do not answer yet.' # shown to be best on CPC
 
-cot_extract = 'Think through the question but do not answer yet.'
-
-def perform_one_token_cpc(llm: LLM, context: str, prompt=cpc_prompt) -> str:
+def perform_one_token_cpc(llm: LLM, context: str, cpc_prompt=cpc_prompt) -> str:
     """Asks the llm to do a one-word completion on whether its priorities should change or not."""
-    return llm.yesno_completion([
+    messages_1t = [
         {
             "role": "assistant",
             "content": context
         },
         {
             "role": "user",
-            "content": prompt + ' ' + yesno_extract
+            "content": cpc_prompt + '\n' + yesno_extract
         }
-    ])
+    ]
+    return llm.yesno_completion(messages_1t)
 
-def perform_cot_cpc(llm: LLM, context: str, prompt=cpc_prompt) -> (str, str):
+def perform_cot_cpc(llm: LLM, context: str, cpc_prompt=cpc_prompt) -> (str, str):
     """
     Asks the llm to make a more lengthy consideration of whether its priorities should change or not.
     :returns: a tuple of two strings; the first string is the llm's thoughts from the CoT prompt; the second is the
     Yes/No response summarizing the CoT result.
     """
-    cot_response = llm.chat_completion([
+    messages_cot = [
         {
             "role": "assistant",
             "content": context
         },
         {
             "role": "user",
-            "content": prompt + ' ' + cot_extract
+            "content": cpc_prompt + '\n' + cot_extract
         }
-    ])
-    return cot_response, llm.yesno_completion([
-        {
-            "role": "assistant",
-            "content": context
-        },
-        {
-            "role": "user",
-            "content": prompt + ' ' + cot_extract
-        },
-        {
+    ]
+    cot_response = llm.chat_completion(messages_cot)
+    messages_cot_answer = [{
             "role": "assistant",
             "content": cot_response
         },
         {
             "role": "user",
-            "content": prompt + ' ' + yesno_extract
-        },
-    ])
+            "content": cpc_prompt + '\n' + yesno_extract
+        }
+    ]
+    return cot_response, llm.yesno_completion(messages_cot + messages_cot_answer)
